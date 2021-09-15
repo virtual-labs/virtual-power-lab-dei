76b499a59d6d57e8e9693bf92fc3aafaa744eb6e
ajaykanr00 <ajaykant900@gmail.com>
2021-09-15 14:59:58 +0530

    Flash Files Updated

---
+++ src/lab/Dreamweaver/Sim11/script.js
@@ -0,0 +1,30 @@
+function imachange1(){
+	document.getElementById("image1").src="NumericalLL.png";
+	document.getElementById("button1").style.background="burlywood";
+    document.getElementById("button1").style.fontFamily="sans-serif";
+    document.getElementById("button3").style.background="#fff";
+    document.getElementById("button2").style.background="#fff";
+    document.getElementById("button2").style.fontFamily="cursive";
+    document.getElementById("button3").style.fontFamily="cursive";
+}
+
+function imachange2(){
+	document.getElementById("image1").src="NumericalLG.png";
+    document.getElementById("button2").style.background="burlywood";
+    document.getElementById("button2").style.fontFamily="sans-serif";
+    document.getElementById("button3").style.background="#fff";
+    document.getElementById("button1").style.background="#fff";
+    document.getElementById("button3").style.fontFamily="cursive";
+    document.getElementById("button1").style.fontFamily="cursive";
+}
+
+function imachange3(){
+	document.getElementById("image1").src="Nofault.png";
+    document.getElementById("button3").style.background="burlywood";
+    document.getElementById("button3").style.fontFamily="sans-serif";
+    document.getElementById("button1").style.background="#fff";
+    document.getElementById("button2").style.background="#fff";
+    document.getElementById("button1").style.fontFamily="cursive";
+    document.getElementById("button2").style.fontFamily="cursive";
+}
+
---
+++ src/lab/Dreamweaver/sim10/script.js
@@ -0,0 +1,226 @@
+function imachange1() {
+    document.getElementById("image").src = "ima/1.png";
+    document.getElementById("image1").src = "ima/plu/on.png";
+    document.getElementById("button1").style.background = "burlywood";
+    document.getElementById("button2").style.background = "#fff";
+    document.getElementById("button3").style.background = "#fff";
+    document.getElementById("button4").style.background = "#fff";
+    document.getElementById("button5").style.background = "#fff";
+    document.getElementById("button11").style.background = "#fff";
+    document.getElementById("button12").style.background = "#fff";
+    document.getElementById("button13").style.background = "#fff";
+    document.getElementById("button14").style.background = "#fff";
+    document.getElementById("button21").style.background = "#fff";
+    document.getElementById("button22").style.background = "#fff";
+    document.getElementById("button23").style.background = "#fff";
+    document.getElementById("button24").style.background = "#fff";
+    document.getElementById("button31").style.background = "#fff";
+    document.getElementById("button32").style.background = "#fff";
+    document.getElementById("button33").style.background = "#fff";
+    document.getElementById("button34").style.background = "#fff";
+    document.getElementById("button41").style.background = "#fff";
+    document.getElementById("button42").style.background = "#fff";
+    document.getElementById("button43").style.background = "#fff";
+    document.getElementById("button44").style.background = "#fff";
+}
+
+function imachange2() {
+    imachange1();
+    document.getElementById("image").src = "ima/2.png";
+    document.getElementById("image1").src = "ima/plu/on.png";
+    document.getElementById("button1").style.background = "#fff";
+    document.getElementById("button2").style.background = "burlywood";
+    document.getElementById("button3").style.background = "#fff";
+    document.getElementById("button4").style.background = "#fff";
+    document.getElementById("button5").style.background = "#fff";
+}
+
+function imachange3() {
+    imachange1();
+    document.getElementById("image").src = "ima/3.png";
+    document.getElementById("image1").src = "ima/plu/on.png";
+    document.getElementById("button1").style.background = "#fff";
+    document.getElementById("button2").style.background = "#fff";
+    document.getElementById("button3").style.background = "burlywood";
+    document.getElementById("button4").style.background = "#fff";
+    document.getElementById("button5").style.background = "#fff";
+}
+
+function imachange4() {
+    imachange1();
+    document.getElementById("image").src = "ima/4.png";
+    document.getElementById("image1").src = "ima/plu/on.png";
+    document.getElementById("button1").style.background = "#fff";
+    document.getElementById("button2").style.background = "#fff";
+    document.getElementById("button3").style.background = "#fff";
+    document.getElementById("button4").style.background = "burlywood";
+    document.getElementById("button5").style.background = "#fff";
+}
+
+function imachange5() {
+    imachange1();
+    document.getElementById("image").src = "ima/5.png";
+    document.getElementById("image1").src = "ima/plu/on.png";
+    document.getElementById("button1").style.background = "#fff";
+    document.getElementById("button2").style.background = "#fff";
+    document.getElementById("button3").style.background = "#fff";
+    document.getElementById("button4").style.background = "#fff";
+    document.getElementById("button5").style.background = "burlywood";
+}
+
+
+/*-------------------------------------------------------*/
+
+
+
+function fault11() {
+    document.getElementById("image").src = "Fault/Star_Star/LG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button11").style.background = "burlywood";
+    document.getElementById("button12").style.background = "#fff";
+    document.getElementById("button13").style.background = "#fff";
+    document.getElementById("button14").style.background = "#fff";
+}
+
+function fault12() {
+    document.getElementById("image").src = "Fault/Star_Star/LL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button11").style.background = "#fff";
+    document.getElementById("button12").style.background = "burlywood";
+    document.getElementById("button13").style.background = "#fff";
+    document.getElementById("button14").style.background = "#fff";
+}
+
+function fault13() {
+    document.getElementById("image").src = "Fault/Star_Star/LLL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button11").style.background = "#fff";
+    document.getElementById("button12").style.background = "#fff";
+    document.getElementById("button13").style.background = "burlywood";
+    document.getElementById("button14").style.background = "#fff";
+}
+
+function fault14() {
+    document.getElementById("image").src = "Fault/Star_Star/LLLG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button11").style.background = "#fff";
+    document.getElementById("button12").style.background = "#fff";
+    document.getElementById("button13").style.background = "#fff";
+    document.getElementById("button14").style.background = "burlywood";
+}
+
+/*-------------------------------------------------------*/
+
+
+function fault21() {
+    document.getElementById("image").src = "Fault/Star_Delta/LG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button21").style.background = "burlywood";
+    document.getElementById("button22").style.background = "#fff";
+    document.getElementById("button23").style.background = "#fff";
+    document.getElementById("button24").style.background = "#fff";
+}
+
+function fault22() {
+    document.getElementById("image").src = "Fault/Star_Delta/LL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button21").style.background = "#fff";
+    document.getElementById("button22").style.background = "burlywood";
+    document.getElementById("button23").style.background = "#fff";
+    document.getElementById("button24").style.background = "#fff";
+}
+
+function fault23() {
+    document.getElementById("image").src = "Fault/Star_Delta/LLL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button21").style.background = "#fff";
+    document.getElementById("button22").style.background = "#fff";
+    document.getElementById("button23").style.background = "burlywood";
+    document.getElementById("button24").style.background = "#fff";
+}
+
+function fault24() {
+    document.getElementById("image").src = "Fault/Star_Delta/LLLG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button21").style.background = "#fff";
+    document.getElementById("button22").style.background = "#fff";
+    document.getElementById("button23").style.background = "#fff";
+    document.getElementById("button24").style.background = "burlywood";
+}
+
+/*-------------------------------------------------------*/
+
+function fault31() {
+    document.getElementById("image").src = "Fault/Delta_Star/LG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button31").style.background = "burlywood";
+    document.getElementById("button32").style.background = "#fff";
+    document.getElementById("button33").style.background = "#fff";
+    document.getElementById("button34").style.background = "#fff";
+}
+
+function fault32() {
+    document.getElementById("image").src = "Fault/Delta_Star/LL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button31").style.background = "#fff";
+    document.getElementById("button32").style.background = "burlywood";
+    document.getElementById("button33").style.background = "#fff";
+    document.getElementById("button34").style.background = "#fff";
+}
+
+function fault33() {
+    document.getElementById("image").src = "Fault/Delta_Star/LLL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button31").style.background = "#fff";
+    document.getElementById("button32").style.background = "#fff";
+    document.getElementById("button33").style.background = "burlywood";
+    document.getElementById("button34").style.background = "#fff";
+}
+
+function fault34() {
+    document.getElementById("image").src = "Fault/Delta_Star/LLLG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button31").style.background = "#fff";
+    document.getElementById("button32").style.background = "#fff";
+    document.getElementById("button33").style.background = "#fff";
+    document.getElementById("button34").style.background = "burlywood";
+}
+
+
+/*-------------------------------------------------------*/
+
+function fault41() {
+    document.getElementById("image").src = "Fault/Delta_Delta/LG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button41").style.background = "burlywood";
+    document.getElementById("button42").style.background = "#fff";
+    document.getElementById("button43").style.background = "#fff";
+    document.getElementById("button44").style.background = "#fff";
+}
+
+function fault42() {
+    document.getElementById("image").src = "Fault/Delta_Delta/LL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button41").style.background = "#fff";
+    document.getElementById("button42").style.background = "burlywood";
+    document.getElementById("button43").style.background = "#fff";
+    document.getElementById("button44").style.background = "#fff";
+}
+
+function fault43() {
+    document.getElementById("image").src = "Fault/Delta_Delta/LLL.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button41").style.background = "#fff";
+    document.getElementById("button42").style.background = "#fff";
+    document.getElementById("button43").style.background = "burlywood";
+    document.getElementById("button44").style.background = "#fff";
+}
+
+function fault44() {
+    document.getElementById("image").src = "Fault/Delta_Delta/LLLG.png";
+    document.getElementById("image1").src = "ima/plu/off.png";
+    document.getElementById("button41").style.background = "#fff";
+    document.getElementById("button42").style.background = "#fff";
+    document.getElementById("button43").style.background = "#fff";
+    document.getElementById("button44").style.background = "burlywood";
+}
